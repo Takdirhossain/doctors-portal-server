48621901e97a8d39eaa2b76a2a3669ddcb8b092c
takdirhossain <takdirhossain35@gmail.com>
2022-11-17 01:52:46 +0600

    set booking

--- index.js
+++ index.js
@@ -6,7 +6,39 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', async(req, res) => {
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://doctor-portal:CeWZIq00sfJdeK5V@cluster0.eurlfla.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appoinmentOptions = client.db('doctors-portal').collection('appoinmentoption')
+        const bookingscollections = client.db('doctors-portal').collection('bookingscollections')
+        app.get('/appoinmentoption', async (req, res) => {
+            const query = {}
+            const option = await appoinmentOptions.find(query).toArray()
+            res.send(option)
+        })
+        app.post('/bookings', async (req, res) => {
+            const id = req.body
+            console.log(id)
+            const result = await bookingscollections.insertOne(id)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.lor(error))
+
+app.get('/', async (req, res) => {
     res.send("This is doctror portal server")
 })
 
